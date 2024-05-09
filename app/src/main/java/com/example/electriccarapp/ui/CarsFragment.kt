ede67c21c99b30643824f78a949289762d75543f
heberth.vinicius <heberth.vinicius@gmail.com>
2024-05-09 17:09:24 -0300

    Refatoração da classe CarsFragments + Alterar código para receber objeto de uma API e montar o model

--- app/src/main/java/com/example/electriccarapp/ui/CarsFragment.kt
+++ app/src/main/java/com/example/electriccarapp/ui/CarsFragment.kt
@@ -1,21 +1,35 @@
 package com.example.electriccarapp.ui
 
 import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.electriccarapp.R
 import com.example.electriccarapp.data.CarFactory
+import com.example.electriccarapp.domain.Car
 import com.example.electriccarapp.ui.adapter.CarAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import org.json.JSONArray
+import org.json.JSONObject
+import org.json.JSONTokener
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLConnection
 
 class CarsFragment : Fragment() {
     lateinit var carList: RecyclerView
     lateinit var fabCalculate: FloatingActionButton
+
+    var carsArray: ArrayList<Car> = ArrayList()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,8 +40,8 @@ class CarsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        callService()
         setUpViews(view)
-        setUpList()
         setUpListeners()
     }
 
@@ -39,7 +53,7 @@ class CarsFragment : Fragment() {
     }
 
     private fun setUpList() {
-        val adapter = CarAdapter(CarFactory.list)
+        val adapter = CarAdapter(carsArray)
         carList.adapter = adapter
     }
 
@@ -47,6 +61,92 @@ class CarsFragment : Fragment() {
 
         fabCalculate.setOnClickListener{
             startActivity(Intent(context , CalculateAutonomyActivity::class.java))
+        }
+    }
+
+    private fun callService() {
+        val urlBase = "https://heberthvinicius.github.io/APITestElectricCar/cars.json"
+        MyTask().execute(urlBase)
+    }
+
+
+    inner class MyTask : AsyncTask<String, String, String>() {
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            Log.d("MyTask", "Iniciando...")
+        }
+
+        override fun doInBackground(vararg url: String?): String {
+            var urlConnection: HttpURLConnection? = null
+
+            try {
+                val urlBase = URL(url[0])
+
+                urlConnection = urlBase.openConnection() as HttpURLConnection
+                urlConnection.connectTimeout = 60000
+                urlConnection.readTimeout = 60000
+                urlConnection.setRequestProperty(
+                    "Accept",
+                    "application/json"
+                )
+
+                val responseCode = urlConnection.responseCode
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    var response = urlConnection.inputStream.bufferedReader().use { it.readText() }
+                    publishProgress(response)
+                }
+                else {
+                    Log.e("Erro", "Serviço indisponível no momento...")
+                }
+            } catch (ex: Exception) {
+                Log.e("Erro", "Erro ao executar o processamento...")
+            } finally {
+                urlConnection?.disconnect()
+            }
+
+            return " "
+        }
+
+        override fun onProgressUpdate(vararg values: String?) {
+            try {
+                val jsonArray = JSONTokener(values[0]).nextValue() as JSONArray
+
+                for (i in 0 until jsonArray.length()) {
+                    val id = jsonArray.getJSONObject(i).getString("id")
+                    Log.d("ID ->", id)
+
+                    val price = jsonArray.getJSONObject(i).getString("price")
+                    Log.d("Price ->", price)
+
+                    val battery = jsonArray.getJSONObject(i).getString("battery")
+                    Log.d("Battery ->", battery)
+
+                    val power = jsonArray.getJSONObject(i).getString("power")
+                    Log.d("Power ->", power)
+
+                    val recharge = jsonArray.getJSONObject(i).getString("recharge")
+                    Log.d("Recharge ->", recharge)
+
+                    val urlPhoto = jsonArray.getJSONObject(i).getString("urlPhoto")
+                    Log.d("URLPhoto ->", urlPhoto)
+
+                    val model = Car(
+                        id = id.toInt(),
+                        price = price,
+                        battery = battery,
+                        power = power,
+                        recharge = recharge,
+                        urlPhoto = urlPhoto
+                    )
+                    carsArray.add(model)
+                }
+                setUpList()
+
+            }catch (ex: Exception) {
+                Log.e("Erro ->", ex.message.toString())
+            }
         }
     }
 }
